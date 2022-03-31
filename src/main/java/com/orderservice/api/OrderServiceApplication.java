dc42fe96d3de1b7beac71f78d1da35fcbbaae921
naveen <naveenreddym1995@gmail.com>
2022-03-31 12:43:59 +0530

    updated main app

--- src/main/java/com/orderservice/api/OrderServiceApplication.java
+++ src/main/java/com/orderservice/api/OrderServiceApplication.java
@@ -10,6 +10,7 @@ public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
+		System.out.println("main");
 	}
 
 }
